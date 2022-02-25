25e9de457421a34b13ce5124a09e2822511299df
Thalles <thalles.devraiz@gmail.com>
2022-02-25 18:13:24 -0300

    ensure LoginController returns 400 if no password is provided

--- src/presentation/controllers/login-controller.js
+++ src/presentation/controllers/login-controller.js
@@ -1,6 +1,6 @@
 class LoginController {
   handle (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
--- src/presentation/controllers/login-controller.spec.js
+++ src/presentation/controllers/login-controller.spec.js
@@ -11,4 +11,14 @@ describe('Login Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
+  it('should return 400 if no password is provided', () => {
+    const sut = new LoginController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
