ccf285710852004310dd91b83cddddfd4b6bf835
Thalles <thalles.devraiz@gmail.com>
2022-02-26 02:30:05 -0300

    ensuere LoginController calls AuthUseCase with email and password

--- src/presentation/controllers/login-controller.js
+++ src/presentation/controllers/login-controller.js
@@ -2,6 +2,10 @@ const HttpResponse = require('../helpers/http-response')
 const MissingParamError = require('../errors/missing-param-error')
 
 class LoginController {
+  constructor (authUseCase) {
+    this.authUseCase = authUseCase
+  }
+
   handle (httpRequest) {
     if (!httpRequest?.body) {
       return HttpResponse.serverError()
@@ -13,6 +17,7 @@ class LoginController {
     if (!password) {
       return HttpResponse.badRequest(new MissingParamError('password'))
     }
+    this.authUseCase.auth(email, password)
   }
 }
 
--- src/presentation/controllers/login-controller.spec.js
+++ src/presentation/controllers/login-controller.spec.js
@@ -3,31 +3,40 @@ const { badRequest, serverError } = require('../helpers/http-response')
 const LoginController = require('./login-controller')
 
 function makeSut () {
-  return new LoginController()
+  const spyAuthUseCase = { auth: jest.fn() }
+  const sut = new LoginController(spyAuthUseCase)
+  return { sut, spyAuthUseCase }
 }
 
 describe('Login Controller', () => {
   it('should return 400 if no email is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = { body: { password: 'any_password' } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
   it('should return 400 if no password is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = { body: { email: 'any_email@mail.com' } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
   it('should return 500 if no httpRequest is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = sut.handle()
     expect(httpResponse).toEqual(serverError())
   })
   it('should return 500 if httpRequest has no body', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {}
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(serverError())
+  })
+  it('should call AuthUseCase with email and password', () => {
+    const { sut, spyAuthUseCase } = makeSut()
+    const httpRequest = { body: { email: 'any_email@mail.com', password: 'any_password' } }
+    const { email, password } = httpRequest.body
+    sut.handle(httpRequest)
+    expect(spyAuthUseCase.auth).toHaveBeenCalledWith(email, password)
   })
 })
