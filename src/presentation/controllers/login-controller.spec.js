605479464e7acb8bf89737f2c1d13da431aa121f
Thalles <thalles.devraiz@gmail.com>
2022-02-26 01:05:15 -0300

    move sut creation to a factory function

--- src/presentation/controllers/login-controller.spec.js
+++ src/presentation/controllers/login-controller.spec.js
@@ -2,26 +2,30 @@ const MissingParamError = require('../errors/missing-param-error')
 const { badRequest, serverError } = require('../helpers/http-response')
 const LoginController = require('./login-controller')
 
+function makeSut () {
+  return new LoginController()
+}
+
 describe('Login Controller', () => {
   it('should return 400 if no email is provided', () => {
-    const sut = new LoginController()
+    const sut = makeSut()
     const httpRequest = { body: { password: 'any_password' } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
   it('should return 400 if no password is provided', () => {
-    const sut = new LoginController()
+    const sut = makeSut()
     const httpRequest = { body: { email: 'any_email@mail.com' } }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
   it('should return 500 if no httpRequest is provided', () => {
-    const sut = new LoginController()
+    const sut = makeSut()
     const httpResponse = sut.handle()
     expect(httpResponse).toEqual(serverError())
   })
   it('should return 500 if httpRequest has no body', () => {
-    const sut = new LoginController()
+    const sut = makeSut()
     const httpRequest = {}
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse).toEqual(serverError())
