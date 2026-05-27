cb67edd2af93ad4068a7c49926875937053131de
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-05-27 14:53:22 -0300

    refactor: move ok http response to a helper function

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -14,3 +14,10 @@ export function serverError (): HttpResponse {
     body: new ServerError()
   }
 }
+
+export function ok (data: any): HttpResponse {
+  return {
+    statusCode: 200,
+    body: data
+  }
+}
