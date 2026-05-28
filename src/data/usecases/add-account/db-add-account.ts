d143c218ebb185a334867544bc204ffc400899a1
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-05-28 12:00:06 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -12,15 +12,10 @@ export class DbAddAccount implements AddAccount {
 
   async add(addAccountModel: AddAccountModel): Promise<AccountModel> {
     const encryptedPassword = await this.encrypter.encrypt(addAccountModel.password)
-    await this.addAccountRepository.add({
+    const account = await this.addAccountRepository.add({
       ...addAccountModel,
       password: encryptedPassword
     })
-    return new Promise(resolve => resolve({
-      id: 'valid_id',
-      name: 'valid_name',
-      email: 'valid_email',
-      password: 'valid_password'
-    }))
+    return account
   }
 }
