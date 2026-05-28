2a617eea85f896e656da08ff0002918349fd3295
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-05-28 15:43:59 -0300

    refactor: move mongodb methods to a helper

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+declare global {
+  var __MONGO_URI__: string;
+}
+
+export {};
---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from "mongodb"
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  async connect (uri: string): Promise<void> {
+      this.client = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  }
+}
