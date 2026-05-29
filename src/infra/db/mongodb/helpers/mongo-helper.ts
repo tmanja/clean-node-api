b920c5fea0690c093f4b537e511850a4f7c532a2
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-05-29 10:56:31 -0300

    chore: udpate mongodb

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,14 +1,17 @@
-import { MongoClient } from "mongodb"
+import { Collection, MongoClient } from "mongodb"
 
 export const MongoHelper = {
   client: null as unknown as MongoClient,
+
   async connect (uri: string): Promise<void> {
-      this.client = await MongoClient.connect(global.__MONGO_URI__, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    this.client = await MongoClient.connect(uri)
   },
+
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+  
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
