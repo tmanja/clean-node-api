04ef140252eb2abc3b4d674c3ee4550dcdfbca1c
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-06-02 14:55:13 -0300

    refactor: remove unused tests

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -19,12 +19,6 @@ export class SignUpController implements Controller {
       if (validationError) {
         return badRequest(validationError)
       }
-      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
-      }
       const { name, email, password, passwordConfirmation } = httpRequest.body
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
