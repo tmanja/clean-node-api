7118d9c302126a71f29b28793ef77f4f30e38bf8
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-06-03 15:17:32 -0300

    refactor: move sut creation to a factory function

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,16 +2,35 @@ import { MissingParamError } from "../../errors"
 import { Validation } from "./validation"
 import { ValidationComposite } from "./validation-composite"
 
+function makeValidation (): Validation {
+  class ValidationStub implements Validation {
+    validate(input: any): Error | undefined {
+      return undefined
+    }
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite,
+  validationStub: Validation
+}
+
+function makeSut (): SutTypes {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut, 
+    validationStub
+  }
+}
+
 describe('Validation Composite', () => {
   test('should return an error if any validation fails', () => {
-    class ValidationStub implements Validation {
-      validate(input: any): Error | undefined {
-        return new MissingParamError('anyField')
-      }
-    }
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const { sut, validationStub } = makeSut()
+    const missingParamError = new MissingParamError('anyField')
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(missingParamError)
     const validationError = sut.validate({ anyField: 'any_value' })
-    expect(validationError).toEqual(new MissingParamError('anyField'))
+    expect(validationError).toEqual(missingParamError)
   })
 })
