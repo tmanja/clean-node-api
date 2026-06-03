671bebd28c44d89d95f67543d77ef8f9a0bc714e
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-06-03 14:11:28 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,26 @@
+import { InvalidParamError, MissingParamError } from "../../errors"
+import { CompareFieldsValidation } from "./compare-fields-validation"
+
+function makeSut (): CompareFieldsValidation {
+  return new CompareFieldsValidation('anyField', 'anyFieldToCompare')
+}
+
+describe('CompareFields Validation', () => {
+  test('should return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+    const validationError = sut.validate({
+      anyField: 'any_value',
+      anyFieldToCompare: 'another_value'
+    })
+    expect(validationError).toEqual(new InvalidParamError('anyFieldToCompare'))
+  })
+
+  test('should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const validationError = sut.validate({
+      anyField: 'any_value',
+      anyFieldToCompare: 'any_value'
+    })
+    expect(validationError).toBeFalsy()
+  })
+})
