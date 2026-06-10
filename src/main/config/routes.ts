e74ba0762403cd872585e730acc7ecfc7241f9c3
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-06-10 16:22:49 -0300

    refactor: make routes import manual

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,8 @@
 import { Express, Router } from "express";
-import FastGlob from "fast-glob";
-
+import loginRoutes from "../routes/login-routes";
 
 export default function (app: Express): void {
   const router = Router()
   app.use('/api', router)
-  FastGlob.sync('**/src/main/routes/**routes.ts').map(async file => {
-    const route = (await import(`../../../${file}`)).default
-    route(router)
-  })
+  loginRoutes(router)
 }
