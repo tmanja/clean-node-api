db61ef94383d402b5651f4cd0d748bcdbc0bdb36
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-06-26 11:31:03 -0300

    chore: adjust jest config

--- jest.config.js
+++ jest.config.js
@@ -175,7 +175,7 @@ const config = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '.+\\.ts$': 'ts-jest',
+    '.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
