128fb9eb8f380bb26e52cc730f4f0d40ee55b967
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-07-03 10:35:49 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct values

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,13 +1,18 @@
 import { LoadAccountByToken } from "../../../domain/usecases/load-account-by-token";
 import { Decrypter } from "../../protocols/cryptography/decrypter";
+import { LoadAccountByTokenRepository } from "../../protocols/db/account/load-account-by-token-repository copy";
 import { AccountModel } from "../add-account/db-add-account-protocols";
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (
-    private readonly decrypter: Decrypter
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
   ) {}
   async load(accessToken: string, role?: string): Promise<AccountModel | null> {
-    this.decrypter.decrypt(accessToken)
+    const token = this.decrypter.decrypt(accessToken)
+    if (token) {
+      this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+    }
     return Promise.resolve(null)
   }
 }
