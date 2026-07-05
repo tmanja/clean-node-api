ca5a392bc46cb6aa455468c72432eeea85f814ab
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-07-05 12:39:19 -0300

    feat: ensure SurveyMongoRepository loads all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,17 @@
+import { LoadSurveysRepository } from "../../../../data/protocols/db/survey/load-surveys-repository";
 import { AddSurveyRepository, Survey } from "../../../../data/usecases/add-survey/db-add-survey-protocols";
+import { SurveyModel } from "../../../../domain/models/survey";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add(survey: Survey): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(survey)
+  }
+
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return surveys.map(MongoHelper.map<SurveyModel>)
   }
 }
