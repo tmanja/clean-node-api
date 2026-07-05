f43b8c7fb3674d777b77ac4c29027a029e264295
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-07-05 15:04:03 -0300

    test: ensure load surveys route returns 403 when access token is not provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -70,4 +70,13 @@ describe('Survey Routes ', () => {
       .expect(204)
     })
   })
+
+  describe('GET /surveys', () => {
+    test('should return 403 on load surveys without token', async () => {
+      await request(app)
+      .get('/api/surveys')
+      .send()
+      .expect(403)
+    })
+  })
 })
