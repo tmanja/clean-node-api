291533b27e9058098810202c7a8b5a4bd099340f
Thalles Manjaterra <thalles.devraiz@gmail.com>
2026-07-06 12:14:25 -0300

    refactor: move access token creation to a factory function

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,9 @@
 import request from 'supertest'
 import app from '../config/app'
-import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
-import { Collection } from 'mongodb';
-import { sign } from 'jsonwebtoken';
-import env from '../config/env';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 describe('Survey Routes ', () => {
   let surveyCollection: Collection
@@ -11,7 +11,7 @@ describe('Survey Routes ', () => {
 
   beforeAll(async () => {
     await MongoHelper.connect(global.__MONGO_URI__)
-  });
+  })
 
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
@@ -22,118 +22,112 @@ describe('Survey Routes ', () => {
 
   afterAll(async () => {
     await MongoHelper.disconnect()
-  });
-  
+  })
+
+  async function makeAccessToken(): Promise<string> {
+    const { insertedId } = await accountCollection.insertOne({
+      name: 'Thalles',
+      email: 'thalles.manjaterra@gmail.com',
+      password: '123',
+      role: 'admin',
+    })
+    const accessToken = sign({ id: insertedId }, env.jwtSecret)
+    await accountCollection.updateOne(
+      {
+        _id: insertedId,
+      },
+      {
+        $set: {
+          accessToken,
+        },
+      },
+    )
+    return accessToken
+  }
+
   describe('POST /surveys', () => {
     test('should return 403 on add survey without token', async () => {
       await request(app)
-      .post('/api/surveys')
-      .send({
-        question: 'Question',
-        answers: [{
-          image: 'http://image-name.com',
-          answer: 'Answer 1'
-        }, {
-          answer: 'Answer 2'
-        }]
-      })
-      .expect(403)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              image: 'http://image-name.com',
+              answer: 'Answer 1',
+            },
+            {
+              answer: 'Answer 2',
+            },
+          ],
+        })
+        .expect(403)
     })
 
     test('should return 204 on add survey with valid token', async () => {
-      const { insertedId } = await accountCollection.insertOne({
-        name: 'Thalles',
-        email: 'thalles.manjaterra@gmail.com',
-        password: '123',
-        role: 'admin'
-      })
-      const accessToken = sign({ id: insertedId }, env.jwtSecret)
-      await accountCollection.updateOne({ 
-        _id: insertedId
-      }, {
-        $set: { 
-          accessToken
-        }
-      })
+      const accessToken = await makeAccessToken()
       await request(app)
-      .post('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'Question',
-        answers: [{
-          image: 'http://image-name.com',
-          answer: 'Answer 1'
-        }, {
-          answer: 'Answer 2'
-        }]
-      })
-      .expect(204)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              image: 'http://image-name.com',
+              answer: 'Answer 1',
+            },
+            {
+              answer: 'Answer 2',
+            },
+          ],
+        })
+        .expect(204)
     })
   })
 
   describe('GET /surveys', () => {
     test('should return 403 on load surveys without token', async () => {
-      await request(app)
-      .get('/api/surveys')
-      .send()
-      .expect(403)
+      await request(app).get('/api/surveys').send().expect(403)
     })
 
     test('should return 200 on load surveys with valid token', async () => {
-      await surveyCollection.insertMany([{
-        question: 'any_question',
-        answers: [{
-          image: 'any_image',
-          answer: 'any_answer'
-        }],
-        date: new Date()
-      }, {
-        question: 'another_question',
-        answers: [{
-          image: 'another_image',
-          answer: 'another_answer'
-        }],
-        date: new Date()
-      }])
-      const { insertedId } = await accountCollection.insertOne({
-        name: 'Thalles',
-        email: 'thalles.manjaterra@gmail.com',
-        password: '123'
-      })
-      const accessToken = sign({ id: insertedId }, env.jwtSecret)
-      await accountCollection.updateOne({
-        _id: insertedId
-      }, {
-        $set: {
-          accessToken
-        }
-      })
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [
+            {
+              image: 'any_image',
+              answer: 'any_answer',
+            },
+          ],
+          date: new Date(),
+        },
+        {
+          question: 'another_question',
+          answers: [
+            {
+              image: 'another_image',
+              answer: 'another_answer',
+            },
+          ],
+          date: new Date(),
+        },
+      ])
+      const accessToken = await makeAccessToken()
       await request(app)
-      .get('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send()
-      .expect(200)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send()
+        .expect(200)
     })
 
     test('should return 204 when there are no surveys', async () => {
-      const { insertedId } = await accountCollection.insertOne({
-        name: 'Thalles',
-        email: 'thalles.manjaterra@gmail.com',
-        password: '123'
-      })
-      const accessToken = sign({ id: insertedId }, env.jwtSecret)
-      await accountCollection.updateOne({
-        _id: insertedId
-      }, {
-        $set: {
-          accessToken
-        }
-      })
+      const accessToken = await makeAccessToken()
       await request(app)
-      .get('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send()
-      .expect(204)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send()
+        .expect(204)
     })
   })
 })
